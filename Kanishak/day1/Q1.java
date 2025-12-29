d309814456ed00f3c5e659f97d5ad2b1b439c3b9
Kannu20 <kannutodwal@gmail.com>
2025-12-29 16:56:17 +0530

    Added kanishak folder

---
+++ Kanishak/day1/Q1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        // ps - prefix sum
+        int[] ps = new int[nums.length];
+        ps[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ps[i] = ps[i-1] + nums[i];
+        }
+        return ps;
+    }
+}
