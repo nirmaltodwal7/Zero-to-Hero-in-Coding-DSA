3084fadab8ac3d9c78841fa8ba17f62777fab3cf
nirmaltodwal <nirmaltodwal566@ gmail.com>
2026-01-08 23:43:57 +0530

    Added Nirmal day11 folder

---
+++ Nirmal/day11/solution.java
@@ -0,0 +1,17 @@
+//  215. Kth Largest Element in an Array //
+
+import java.util.PriorityQueue; 
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
